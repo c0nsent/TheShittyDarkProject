0a18dca2033f21cb1c495ee2f2ae175546f1caec
consent <c0nsent@proton.me>
2026-01-05 09:13:03 +0300

    Update CMakeLists.txt to include glm and refactor main.cpp for GLFW initialization and input handling

--- main.cpp
+++ main.cpp
@@ -1,18 +1,83 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <iostream>
 #include <cstdint>
+#include <iostream>
+#include <string>
+#include <bits/codecvt.h>
+
+using u8 = std::uint8_t;
+using i32 = std::int32_t;
+using u32 = std::uint32_t;
+using u16 = std::uint16_t;
+
+constexpr u16 c_width = 640;
+constexpr u16 c_height = 480;
+
+struct Color
+{
+	float red, green, blue, alpha;
+};
+
+void errorCallback(const i32 error, const char *description)
+{
+	std::cerr << "Error: "  << error << " - " << description << std::endl;
+}
+
+void framebufferSizeCallback(GLFWwindow *window, const i32 width, const i32 height)
+{
+	glViewport(0, 0, width, height);
+}
+
+[[nodiscard]] bool initGlfw(const u8 majorVersion, const u8 minorVersion, const bool isCoreProfile)
+{
+	if (not glfwInit()) return false;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, majorVersion);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minorVersion);
+
+	const auto openglProfile{isCoreProfile ? GLFW_OPENGL_CORE_PROFILE : GLFW_OPENGL_COMPAT_PROFILE};
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, openglProfile);
+
+	return true;
+}
+
+void processInput(GLFWwindow *window, Color &windowFiller)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+	{
+		windowFiller = Color{1.0f, 0.0f, 0.5f, 1.0f};
+	}
+
+
+}
 
 int main()
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwSetErrorCallback(errorCallback);
+
+	/*if (initGlfw(3, 3, true))
+	{
+		std::cerr << "Failed to initialize GLFW." << std::endl;
+		return 1;
+	}*/
+
+
+	if (not glfwInit())
+	{
+		std::cerr << "Failed to initialize GLFW." << std::endl;
+		return 1;
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	auto window{glfwCreateWindow(640, 480, "This Charming Man", NULL, NULL)};
-	if (window == NULL)
+	auto window{glfwCreateWindow(c_width, c_height, "This Charming Man", nullptr, nullptr)};
+	if (window == nullptr)
 	{
 		std::cerr << "Failed to create GLFW window." << std::endl;
 		glfwTerminate();
@@ -25,4 +90,21 @@ int main()
 		std::cerr << "Failed to initialize GLAD." << std::endl;
 		return 1;
 	}
+
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	while (not glfwWindowShouldClose(window))
+	{
+		Color windowFiller{0.2f, 0.3f, 0.3f, 1.0f};
+		processInput(window, windowFiller);
+
+		glClearColor(windowFiller.red, windowFiller.green, windowFiller.blue, windowFiller.alpha);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
